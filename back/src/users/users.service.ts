b9e1a41a61efb97cb3b31deb0b6561367b11316f
kersolo <loick.ferelloc@gmail.com>
2025-06-07 11:59:45 +0200

    test crud post user ok

---
+++ back/src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    await this.prismaService.user.create({ data: createUserDto });
+    return `L'utilisateur ${createUserDto.name} a bien été créé`;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
